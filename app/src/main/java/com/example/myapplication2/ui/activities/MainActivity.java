a2c7b4228bbb354a315ba41be79f1406622c9620
OldestPirate <s.pizzini.98@gmail.com>
2019-01-31 17:40:46 +0100

    quarto commit

--- app/src/main/java/com/example/myapplication2/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/ui/activities/MainActivity.java
@@ -1,4 +1,36 @@
 package com.example.myapplication2.ui.activities;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.myapplication2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.login_menu) {
+            startActivity(new Intent(this, LoginActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
